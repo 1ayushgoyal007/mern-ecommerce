d0da5bbec10827769be0dd601b33fc71bbbe1eb0
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-11-06 21:40:35 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,28 @@ dotenv.config();
 connectDB();
 
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static( path.join(__dirname, '/uploads') ))
+
+
+if(process.env.NODE_ENV === 'production'){
+    console.log('i was called 1');
+    console.log('dirname', __dirname);
+    console.log('i was called 12');
+
+    app.use(express.static( path.join( __dirname, 'frontend','build' ) ));
+    const newPath = path.join( __dirname, 'frontend','build');
+    console.log(newPath);
+    app.get('/', (req, res)=> {
+        console.log('I was called');
+        res.sendFile( path.resolve( __dirname, 'frontend', 'build', 'index.html' ) )
+    })
+}else{
+    app.get('/', function(req,res){
+        res.send('running');
+    })
+
+}
 
 
 
@@ -30,24 +52,10 @@ app.use('/api/upload',uploadRoutes);
 
 app.get('/api/config/paypal', (req,res)=> res.send( process.env.PAYPAL_CLIENT_ID ) )
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static( path.join(__dirname, '/uploads') ))
+
 app.use(notFound);
 
-if(process.env.NODE_ENV === 'production'){
-    console.log('i was called 1');
 
-    app.use(express.static( path.join( __dirname, '/frontend/build' ) ));
-    app.get('*', (req, res)=> {
-        console.log('i was called');
-        res.sendFile( path.resolve( __dirname, 'frontend', 'build', 'index.html' ) )
-    })
-}else{
-    app.get('/', function(req,res){
-        res.send('running');
-    })
-
-}
 
 const PORT = process.env.PORT || 5000;
 
